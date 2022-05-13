c68f4b3aea8c0e980802a3a9acd49f6577c12589
MacSach <96050982+MacSach@users.noreply.github.com>
2022-05-13 23:00:08 +0200

    Update MSsenat - BS4.py

--- soup/MSsenat - BS4.py
+++ soup/MSsenat - BS4.py
@@ -17,10 +17,11 @@ limit = True
 
 ###################### LINKS PART 1 ####################################
 
-
-
 if limit == 1:
     Limit = 100 
+   
+# The "start" page. On this page we can find all links that will be useful for our analysis. 
+# This code scraps all available links on this page. To do that, it uses "links" to save https links and "links2" to scrap http links.
 
 url= 'https://www.senat.gov.pl/prace/posiedzenia/tematy-posiedzen-senatu-ix-kadencji'
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -41,6 +42,7 @@ for link in links2:
 
 print(linksA)
 
+# Filtering scrapped links to left only that will be useful for our analysis.
 linksAA = []
 for l in linksA:
     if ("posiedzenia" in str(l)) and ("senat/posiedzenia" not in str(l)) and (l not in linksAA):
@@ -54,8 +56,10 @@ len(linksAA)
 linksAAA = [s for s in linksAA if "prace/posiedzenia" in s]
 linksAAA
 len(linksAAA)
+# Filtering finished succesfully.
 
 ###################### LINKS PART 2 ####################################
+# This part gathers links to voting's results pages from links gathered during first stage.
 
 linksB = []
 for lB in linksAAA:
@@ -67,6 +71,8 @@ len(linksB)
 
 
 ###################### LINKS PART 3 ####################################
+# During this stage, code saves direct links to print views of the voting results. One link from the stage 2 can provide more than one link so
+# the limit option could be useful here.
 
 linksCC = []
 
@@ -92,6 +98,9 @@ else:
 len(linksCCCC)
 
 ########################### VOTES ###########################################
+# At this stage we gather information that is interesting for us - voting results.
+# We save at "names" names of the senators, at "voting" voting results for specific senator (yes, no, didn't participate during specific voting),
+# at "glosowanie" we save link to the voting result to manage filtering options during post-scrapping analysis.
 
 names = []
 voting = []
@@ -121,6 +130,7 @@ for urlD in linksCCCC:
     print(D)
 
 ###################### EXCEL GENERATION #######################################
+# We save our results in Excel table. Now those results are ready for analysis and easy to read.
 
 if limit == 1:
     D.to_excel('glosowanko100.xlsx', encoding="utf-8", index=False)
